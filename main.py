58e841911c24ac170418987ad9fe104117363263
Chelsea W <80985676+cheollie@users.noreply.github.com>
2021-08-29 08:30:37 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-mysp=__import__("my-voice-analysis")
-
-p="" # add file name WITHOUT file type. E.g: if you have sample.wav, type sample
-c=r"" # add file's directory
-
-print("pace = " + mysp.pace(p, c))
-print("tone = " + mysp.tone(p, c))
-print("filler = " + mysp.fillers(p + ".wav"))
-print("length (hrs, min, sec) = " + str(mysp.outlength(p + ".wav")))
